36fe6ab85f198264c013a368bf29203ca85e2144
Matthew Hayhurst <Matthew.Hayhurst@gmail.com>
2019-01-08 21:42:17 +0000

    small tweaks

---
+++ WpfTestApp/ViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTestApp
+{
+    public class ViewModel
+    {
+
+        public string FilterText { get; set; }
+    }
+}
