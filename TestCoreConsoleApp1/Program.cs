ff0a7c2e7946ee34c6ec61fbdf49b48927f31a53
MatthewHays <Matthew.Hayhurst@gmail.com>
2021-01-29 21:23:47 +0000

    blah

--- TestCoreConsoleApp1/Program.cs
+++ TestCoreConsoleApp1/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace TestCoreConsoleApp1
@@ -7,18 +12,49 @@ namespace TestCoreConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World 1!");
+            ServiceCollection serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+
+            var options = serviceProvider.GetService < IOptions < AppConfig >>();
+
+            /*Console.WriteLine("Hello World 1!");
 
             //await MyMethod();
 
             Console.WriteLine("Hello World 2!");
 
-            Bob().ContinueWith((_) => Console.WriteLine(_.Result)).Wait();
+            Bob().ContinueWith((_) => Console.WriteLine(_.Result)).Wait();*/
             //Task.Factory.StartNew()
 
             Console.ReadLine();
         }
+        class AppConfig
+        {
+            public Dictionary<string,string> values { get;set;}
+        }
 
+        private static void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            // Build configuration
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+
+            var a = configuration.GetSection("values");
+            
+            // Add access to generic IConfigurationRoot
+            serviceCollection.AddSingleton<IConfigurationRoot>(configuration);
+
+            
+            serviceCollection.AddOptions();
+            serviceCollection.Configure<AppConfig>(configuration);
+
+            // Add app
+            //serviceCollection.AddTransient<App>();
+
+        }
         private static async Task MyMethod()
         {
             await Task.Delay(10);
